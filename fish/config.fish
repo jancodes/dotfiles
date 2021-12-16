230b7761b9d1cc6d43006d79f36c923c4fbb31ff
Jansen <jansenli@gmail.com>
2021-12-15 19:21:06 -0500

    sync

---
+++ fish/config.fish
@@ -0,0 +1,46 @@
+# install fisher as plugin manager
+# fisher plugins - bobthefish (theme), z (file history),
+# done (task timer), fast-nvm-fish
+set -g theme_nerd_fonts yes
+
+set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/Cellar/watchman/4.9.0_3/bin" $fish_user_paths
+
+alias ..="cd .."
+
+set -x NODE_ENV development
+set -x EDITOR "nvim"
+set -x CLICOLOR 1
+set -x LSCOLORS "ExFxBxDxCxegedabagacad"
+
+# windows only
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+# Functions
+function createJSConfig
+  cp ~/.config/jsconfig.json . 
+end
+
+function config
+  cd ~/.config
+end
+
+function cfish 
+  nvim ~/.config/fish/config.fish
+end
+
+function cnvim
+  nvim ~/.config/nvim/init.vim
+end
+
+function reload
+  source ~/.config/fish/config.fish
+end
+
+function coverage
+  open ./test-results/coverage/lcov-report/index.html
+end
+
+function clean
+  git reset --soft HEAD~$argv[1]
+end
