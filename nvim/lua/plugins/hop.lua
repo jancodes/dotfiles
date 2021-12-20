248c7d137110013e7d91cedf08d13ed052a57614
Jansen <jancodes@users.noreply.github.com>
2021-12-19 19:15:01 -0500

    sync

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -3,5 +3,5 @@ require('hop').setup ({
     keys = 'etovxqpdygfblzhckisuran'
 })
 
-u.nmap("h","<cmd>lua require'hop'.hint_words()<cr>" )
+u.nmap("h","<cmd>lua require'hop'.hint_char1()<cr>" )
 u.nmap("l","<cmd>lua require'hop'.hint_lines()<cr>" )
