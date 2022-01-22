a84e2a4a14859c99e40bdf9b6ed2590971448014
jancodes <jancodes@users.noreply.github.com>
2022-01-22 14:29:37 -0500

    sync

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,18 @@
-local packer = require("packer")
+local vim = vim
+
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+	execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
+end
+
+vim.cmd('packadd packer.nvim')
+
+local packer = require'packer'
 
 return packer.startup(function(use)
     use "wbthomason/packer.nvim"
