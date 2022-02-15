0164f51299d6725dac759dbd7e48d46124817a54
jancodes <jancodes@users.noreply.github.com>
2022-02-15 17:06:15 -0500

    sync

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,2 +1,9 @@
-require('telescope').load_extension('live_grep_raw')
+local telescope = require('telescope')
 
+telescope.load_extension('live_grep_raw')
+
+telescope.setup({
+    defaults = {
+        file_ignore_patterns = { "node_modules" }
+    }
+})
