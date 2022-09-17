f1dcb380500cde1aa7cf313926a153f0eccab303
jancodes <jancodes@users.noreply.github.com>
2022-09-16 22:25:07 -0400

    sync

--- nvim/lua/plugins/rooter.lua
+++ nvim/lua/plugins/rooter.lua
@@ -1,5 +1,5 @@
 require('nvim-rooter').setup {
-  rooter_patterns = { '.git', 'package.json', 'node_modules' },
+  rooter_patterns = { '.git', 'package.json', 'node_modules', 'init.lua' },
   trigger_patterns = { '*' },
   manual = false,
 }
