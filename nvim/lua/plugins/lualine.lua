d2c07936ab2a785ceb7f120f494be2c568965227
jancodes <jancodes@users.noreply.github.com>
2022-10-02 19:54:41 -0400

    sync

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -11,27 +11,42 @@ require'lualine'.setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     -- S means paused, $ means recording
-    lualine_c = {'filename'},
-    lualine_x = {'filetype'},
+    lualine_c = { {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    } },
+    lualine_x = {
+      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
+        hint = ' ' } },
+      'encoding',
+      'filetype'
+    },
     lualine_y = {
-      function()
-        if vim.g.persisting then
-          return "   "
-        else
-          return "   "
-        end
-      end,
+      -- Don't need this now since I always start nvim with no args
+      -- function()
+      --   if vim.g.persisting then
+      --     return "   "
+      --   else
+      --     return "   "
+      --   end
+      -- end,
+      'progress'
     },
     lualine_z = {'location'}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = { {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    } },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  extensions = {}
+  extensions = {'fugitive'}
 }
