b95c425ef86b75511f7e4fd6b2d3151130aa8dc2
jancodes <jancodes@users.noreply.github.com>
2022-10-02 21:04:16 -0400

    sync

--- nvim/lua/lsp/lsp.lua
+++ nvim/lua/lsp/lsp.lua
@@ -139,4 +139,28 @@ lspconfig.omnisharp.setup({
     on_attach = on_attach
 })
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  update_in_insert = false,
+  virtual_text = { spacing = 4, prefix = "●" },
+  severity_sort = true,
+}
+)
 
+-- Diagnostic symbols in the sign column (gutter)
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●'
+  },
+  update_in_insert = true,
+  float = {
+    source = "always", -- Or "if_many"
+  },
+})
