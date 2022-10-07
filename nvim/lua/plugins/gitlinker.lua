061fda053437c7cc073ff280f9c3116c2b5edeaa
jancodes <jancodes@users.noreply.github.com>
2022-10-06 21:43:29 -0400

    sync

--- nvim/lua/plugins/gitlinker.lua
+++ nvim/lua/plugins/gitlinker.lua
@@ -1,6 +1,37 @@
+-- Build and return the Azure DevOps url.
+---@param url_data table
+---@return string
+local function get_azure_devops_url(url_data)
+  if url_data.host == 'ssh.dev.azure.com' then
+    -- Add the `_git` value right before the repository which is after the last
+    -- forward slash. This is already present if the host is `https`. Also,
+    -- remove the `v3/` part at the beginning which is the ssh version for
+    -- Azure DevOps.
+    url_data.repo = url_data.repo:gsub('/([^/]+)$', '/_git/%1'):gsub('^v3/', '')
+  end
+
+  local url = 'https://dev.azure.com/' .. url_data.repo
+  if not (url_data.file and url_data.rev) then
+    return url
+  end
+
+  url = ('%s?version=GC%s&path=%s'):format(url, url_data.rev, url_data.file)
+  if not url_data.lstart or not url_data.lend then
+    return url
+  end
+
+  url = ('%s&line=%d&lineEnd=%d&lineStartColumn=1&lineEndColumn=1&lineStyle=plain&_a=contents'):format(
+    url,
+    url_data.lstart,
+    url_data.lend + 1
+  )
+
+  return url
+end
+
 require"gitlinker".setup({
   callbacks = {
-    ["github.aexp.com"] = require"gitlinker.hosts".get_github_type_url
+    ["dev.azure.com"] = get_azure_devops_url,
   },
   opts = {
     remote = nil, -- force the use of a specific remote
