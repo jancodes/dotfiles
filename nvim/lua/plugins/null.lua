249ccf54a107616e436c78f95f9ec95f081f08cf
jancodes <jancodes@users.noreply.github.com>
2023-01-31 22:15:08 -0500

    sync

--- nvim/lua/plugins/null.lua
+++ nvim/lua/plugins/null.lua
@@ -1,3 +1,4 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 return {
     'jose-elias-alvarez/null-ls.nvim',
     config = function()
@@ -60,9 +61,21 @@ return {
                     end
                 end
             },
-            on_attach = function(client)
-                if client.server_capabilities.documentFormattingProvider then
-                    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr, })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({
+                                bufnr = bufnr,
+                                filter = function(client)
+                                    return client.name == 'null-ls'
+                                end
+                        })
+                        end,
+                    })
                 end
             end,
         })
--- nvim/lua/plugins/persisted.lua
+++ nvim/lua/plugins/persisted.lua
@@ -6,6 +6,9 @@ return {
   config = function()
     require("persisted").setup({
       autoload = true,
+      on_autoload_no_session = function()
+        vim.notify("No existing session to load.")
+      end
     })
     require("telescope").load_extension("persisted") -- To load the telescope extension
   end
