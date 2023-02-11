e72820417d90d9eb2b1cff810e266815f5c733e6
jancodes <jancodes@users.noreply.github.com>
2023-02-10 21:10:00 -0500

    sync

---
+++ nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,13 @@
+return {
+  "glepnir/lspsaga.nvim",
+  event = "BufRead",
+  config = function()
+    require("lspsaga").setup({
+      symbol_in_winbar = {
+        enable = false
+      }
+    })
+  end,
+  dependencies = { { "nvim-tree/nvim-web-devicons" } }
+}
+
---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,27 @@
+return {
+  "stevearc/oil.nvim",
+  config = function()
+    require("oil").setup({
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["g."] = "actions.toggle_hidden",
+      },
+      view_options = {
+        show_hidden = true,
+      }
+    })
+
+    vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+  end
+}
