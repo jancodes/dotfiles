4a0419992ea29f9d3cfed5e0289b8c2bfe22f0db
jancodes <jancodes@users.noreply.github.com>
2023-04-28 21:23:16 -0400

    sync

---
+++ nvim/lua/plugins/tmux-nav.lua
@@ -0,0 +1,17 @@
+return {
+  'alexghergh/nvim-tmux-navigation',
+  config = function()
+    require 'nvim-tmux-navigation'.setup {
+      disable_when_zoomed = true,       -- defaults to false
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      }
+    }
+  end,
+  lazy = false
+}
