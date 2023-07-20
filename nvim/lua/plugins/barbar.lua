bd531299630d8de519627c84a52b0021cdb6c165
jancodes <jancodes@users.noreply.github.com>
2023-07-20 11:36:10 -0400

    update barbar and fix pinned name issue

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -6,18 +6,8 @@ return {
       exclude_name = { 'profile.log' },
       excluded_buftypes = { 'terminal' },
       icons = {
-        buffer_index = false,
-        buffer_number = false,
-        button = 'X',
-        diagnostics = {},
-        filetype = { enabled = true },
-        inactive = { separator = { left = '▎', right = '' } },
-        modified = { button = '●' },
-        pinned = { button = '📌' },
-        separator = { left = '▎', right = '' },
+        pinned = { button = '📌', filename = true },
       },
     }
   end,
-  -- icons broken in the barbar refactor
-  commit = 'b5b9888'
 }
