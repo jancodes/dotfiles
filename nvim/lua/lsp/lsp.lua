eb2de56753147f4da58ff79a63d2689c810a81e3
Jansen Li <jancodes@users.noreply.github.com>
2023-11-06 21:57:43 -0500

    sync

--- nvim/lua/lsp/lsp.lua
+++ nvim/lua/lsp/lsp.lua
@@ -77,19 +77,19 @@ end
 -- })
 
 lspconfig.cssls.setup({
-    on_attach = function(client, bufnr)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-        on_attach(client, bufnr)
-    end,
+    -- on_attach = function(client, bufnr)
+    --     client.server_capabilities.documentFormattingProvider = false
+    --     client.server_capabilities.documentRangeFormattingProvider = false
+    --     on_attach(client, bufnr)
+    -- end,
 })
 
 lspconfig.jsonls.setup({
-    on_attach = function(client, bufnr)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-        on_attach(client, bufnr)
-    end,
+    -- on_attach = function(client, bufnr)
+    --     client.server_capabilities.documentFormattingProvider = false
+    --     client.server_capabilities.documentRangeFormattingProvider = false
+    --     on_attach(client, bufnr)
+    -- end,
 })
 
 -- lspconfig.tailwindcss.setup {
