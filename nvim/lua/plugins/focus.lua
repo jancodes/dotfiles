21a355f7c21c7ba51850d42b0e879421fe4f6760
Jansen Li <jancodes@users.noreply.github.com>
2023-11-12 21:38:42 -0500

    sync

--- nvim/lua/plugins/focus.lua
+++ nvim/lua/plugins/focus.lua
@@ -7,8 +7,8 @@ return {
                 enable = true,        -- Enable or disable auto-resizing of splits
                 width = 0,            -- Force width for the focused window
                 height = 0,           -- Force height for the focused window
-                minwidth = 0,         -- Force minimum width for the unfocused window
-                minheight = 0,        -- Force minimum height for the unfocused window
+                minwidth = 50,        -- Force minimum width for the unfocused window
+                minheight = 10,       -- Force minimum height for the unfocused window
                 height_quickfix = 10, -- Set the height of quickfix panel
             },
         })
