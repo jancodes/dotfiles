d8716db1750b8ed129f426db573a2770320d20f2
Jansen Li <jancodes@users.noreply.github.com>
2023-11-14 17:51:19 -0500

    sync

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -6,10 +6,10 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      javascript = { "eslint" },
-      typescript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescriptreact = { "eslint" },
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
