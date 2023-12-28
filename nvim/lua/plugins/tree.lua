09c7088f2c6541e3c03bdb9711fe028b8dfbc248
Jansen Li <jancodes@users.noreply.github.com>
2023-12-28 00:25:57 -0500

    sync

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -2,7 +2,6 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   config = function()
-
     require('nvim-treesitter.configs').setup {
       ensure_installed = { "javascript", "typescript", "tsx", "html", "css", "scss", "json", "yaml", "lua", "bash", "dockerfile", "vim", "python", "c", "c_sharp", "cmake", "git_config", "git_rebase", "gitcommit", "gitignore", "go", "graphql", "java", "jsdoc", "make", "vimdoc", "yaml" },
       ignore_install = { "phpdoc", "php" },
@@ -12,9 +11,6 @@ return {
       },
       indent = {
         enable = false,
-      },
-      context_commentstring = {
-        enable = true
       },
       autotag = {
         enable = true
