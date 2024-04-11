df9de2848190c57e398a0d3d52412579c91e6308
Jansen Li <jancodes@users.noreply.github.com>
2024-04-11 11:26:49 -0400

    sync

--- nvim/after/ftplugin/json.lua
+++ nvim/after/ftplugin/json.lua
@@ -2,8 +2,3 @@
 if vim.fn.exists("+winbar") == 1 then
   vim.opt_local.winbar = "%{%v:lua.require'jsonpath'.get()%}"
 end
-
--- send json path to clipboard
-vim.keymap.set("n", "y<C-p>", function()
-  vim.fn.setreg("+", require("jsonpath").get())
-end, { desc = "copy json path", buffer = true })
