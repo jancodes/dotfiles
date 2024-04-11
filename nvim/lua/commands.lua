43eec16eab2bbd8252bf2713ce194c6b1ae02948
Jansen Li <jancodes@users.noreply.github.com>
2024-04-11 10:07:00 -0400

    remove yank paste

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -147,19 +147,5 @@ vim.api.nvim_create_user_command("FormatEnable", function()
   vim.b.disable_autoformat = false
   vim.g.disable_autoformat = false
 end, {
-  desc = "Re-enable autoformat-on-save",
+  desc = "FormatEnable",
 })
-
-local function visual_paste_without_yank()
-  local register = '+'
-
-  if vim.o.clipboard == 'unnamed' then
-    register = '*'
-  end
-
-  vim.fn.setreg('x', vim.fn.getreg(register))
-  vim.api.nvim_paste(vim.fn.getreg(register), {}, -1)
-  vim.fn.setreg(register, vim.fn.getreg('x'))
-end
-
-vim.keymap.set('v', 'p', visual_paste_without_yank, { noremap = true, silent = true })
