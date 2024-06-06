ac144a6871066ac0e7db8d0e73dd20f3e298ed70
Jansen Li <jancodes@users.noreply.github.com>
2024-06-05 21:59:59 -0400

    sync

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,7 +8,7 @@ return {
         enabled = true,
         auto_trigger = true,
         keymap = {
-          accept = false,
+          accept = "<TAB>",
           next = "<A-]>",
           dismiss = "<A-[>",
         }
